8272a011719c42f2982eee8a985a3dd17e3a943d
Will Halling <willhalling@gmail.com>
2019-06-05 12:29:32 +0100

    Update tracking ID.

--- plugins/ga.js
+++ plugins/ga.js
@@ -15,7 +15,7 @@ export default ({ app }) => {
     /*
     ** Set the current page
     */
-    ga('create', 'UA-140807031-1', 'auto')
+    ga('create', 'UA-141525707-1', 'auto')
     /*
     ** Every time the route changes (fired on initialization too)
     */
